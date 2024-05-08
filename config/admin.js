6379a4c394a29ef3974800227d4dd106b092b273
rasyidk <rasid.rasid9961@gmail.com>
2024-05-08 08:57:11 +0000

    add route /be

--- config/admin.js
+++ config/admin.js
@@ -1,3 +1,4 @@
+
 module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
--- config/server.js
+++ config/server.js
@@ -1,5 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
+url: env('BASE_URL', 'http://trpl.tech/be'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
